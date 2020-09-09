ec27ec7a534747e9a5bf6a0453eaf7f7284d149a
micahmosley <micah.mosley@utexas.edu>
2020-09-09 09:08:24 -0500

    added code to render pokemon on page

--- app/assets/config/index.js
+++ app/assets/config/index.js
@@ -58,15 +58,49 @@ function continueConvo(name){
     div.innerHTML=`<p>Wonderful! Trainer ${name} you are about to embark on an exciting but challenging journey. I'm confident that one day you'll be the greatest trainer alive! But first, let's get you some Pokemon.</p>`
     setTimeout(function(){
         div.setAttribute('class', 'hidden')
+
+        //only here until fetch is working
+        renderPokemon()
     }, 5000)
 
-    fetch(Pokemon_URL)
-    .then(resp => resp.json())
-    .then(json => renderPokemon(json));
+    // fetch(Pokemon_URL)
+    // .then(resp => resp.json())
+    // .then(json => renderPokemon(json));
+    
+}
+
+function renderPokemon(){
+    data=[{name: 'squirtle', poke_type: 'water', img: 'pokemon-lite/app/assets/images/squirtle.jpeg'},
+        {name: 'ivysaur', poke_type: 'grass', img: 'pokemon-lite/app/assets/images/ivysaur.jpeg'},
+        {name: 'charizard', poke_type: 'fire', img: 'pokemon-lite/app/assets/images/charizard.jpeg'},
+        {name: 'staryu', poke_type: 'water', img: 'pokemon-lite/app/assets/images/staryu.jpeg'},
+        {name: 'vileplume', poke_type: 'grass', img: 'pokemon-lite/app/assets/images/vileplume.jpeg'},
+        {name: 'vulpix', poke_type: 'fire', img: 'pokemon-lite/app/assets/images/vulpix.jpeg'},
+        {name: 'weepinbell', poke_type: 'grass', img: 'pokemon-lite/app/assets/images/weepinbell.png'},
+        {name: 'rapidash', poke_type: 'fire', img: 'pokemon-lite/app/assets/images/rapidash.png'},
+        {name: 'pikachu', poke_type: 'electric', img: 'pokemon-lite/app/assets/images/pikachu.jpeg'},
+        {name: 'zapdos', poke_type: 'electric', img: 'pokemon-lite/app/assets/images/zapdos.jpeg'},
+        {name: 'magnemite', poke_type: 'electric', img: 'pokemon-lite/app/assets/images/magnemite.jpeg'}
+        ]
+    const types=document.querySelectorAll('ul')
+    data.forEach((pokemon)=> {
+        let li=document.createElement('li')
+        li.innerText=pokemon.name
+        li.setAttribute('type', pokemon.poke_type)
+        li.setAttribute('pic', pokemon.img)
+        types.forEach((type)=>{
+            let id=type.getAttribute('id')
+            if (id==pokemon.poke_type){
+                type.append(li)
+            }
+        })
+        li.addEventListener('click', addPokemon)
+    })
+    let div=document.querySelector('.pokemon')
+    div.setAttribute('class', '')
 
 }
 
-function renderPokemon(data){
-    console.log(data)
-
+function addPokemon(e){
+    console.log(e.target)
 }
--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,10 +1,19 @@
 class PokemonsController < ApplicationController
-
+    
     def index
         pokemon=[{name: 'squirtle', poke_type: 'water', img: 'pokemon-lite/app/assets/images/squirtle.jpeg'},
-        {name: 'ivysaur', poke_type: 'leaf', img: 'pokemon-lite/app/assets/images/ivysaur.jpeg'},
-        {name: 'charizard', poke_type: 'fire', img: 'pokemon-lite/app/assets/images/charizard.jpeg'}
+        {name: 'ivysaur', poke_type: 'grass', img: 'pokemon-lite/app/assets/images/ivysaur.jpeg'},
+        {name: 'charizard', poke_type: 'fire', img: 'pokemon-lite/app/assets/images/charizard.jpeg'},
+        {name: 'staryu', poke_type: 'water', img: 'pokemon-lite/app/assets/images/staryu.jpeg'},
+        {name: 'vileplume', poke_type: 'grass', img: 'pokemon-lite/app/assets/images/vileplume.jpeg'},
+        {name: 'vulpix', poke_type: 'fire', img: 'pokemon-lite/app/assets/images/vulpix.jpeg'},
+        {name: 'weepinbell', poke_type: 'grass', img: 'pokemon-lite/app/assets/images/weepinbell.png'},
+        {name: 'rapidash', poke_type: 'fire', img: 'pokemon-lite/app/assets/images/rapidash.png'},
+        {name: 'pikachu', poke_type: 'electric', img: 'pokemon-lite/app/assets/images/pikachu.jpeg'},
+        {name: 'zapdos', poke_type: 'electric', img: 'pokemon-lite/app/assets/images/zapdos.jpeg'},
+        {name: 'magnemite', poke_type: 'electric', img: 'pokemon-lite/app/assets/images/magnemite.jpeg'}
         ]
+
 
         render json: pokemon
     end 
